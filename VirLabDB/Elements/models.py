cedda57395d8e1992dda8d070b960c3b8580426b
Andres Morales <mora200217@hotmail.com>
2017-03-05 11:29:45 -0500

    Added units charField

--- VirLabDB/Elements/models.py
+++ VirLabDB/Elements/models.py
@@ -3,12 +3,14 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(blank=True, max_length=255)
-    description = models.CharField(blank=True, max_length=25)
+    description = models.CharField(blank=True, max_length=255)
     category = models.CharField(blank=True, max_length=255)
     uncertainty = models.DecimalField(max_digits=100, decimal_places=2)
+    units = models.CharField(blank=True, max_length=10)
     brand = models.CharField(blank=True, max_length=255)
-    quantity = models.IntegerField(blank=True)
-    Available = models.BooleanField(default=True)
+    quantity = models.IntegerField(blank = True)
+    Available = models.BooleanField(default = True)
+    image = models.ImageField(blank = True)
 
     def __str__(self):
         return self.name
