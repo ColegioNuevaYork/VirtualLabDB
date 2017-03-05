7a6603a762936efdc5d7d33eba79d52ee70773e4
Andres Morales <mora200217@hotmail.com>
2017-03-05 12:52:35 -0500

    Static files dirs added to settings. Light changes in html and css files

--- VirLabDB/VirLabDB/settings.py
+++ VirLabDB/VirLabDB/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = 'media'
