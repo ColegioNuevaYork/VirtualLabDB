dc80a2de2e08298619a5c11cd7966843d276739f
Andres Morales <mora200217@hotmail.com>
2017-03-04 20:03:28 -0500

    Added Model. Added App. Created Super User

--- VirLabDB/Elements/admin.py
+++ VirLabDB/Elements/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+# admin.site.register(models.Product)
+@admin.register(models.Product)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'brand', 'uncertainty', 'Available')
+    list_filter = ('category',)
--- VirLabDB/VirLabDB/settings.py
+++ VirLabDB/VirLabDB/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Elements.apps.ElementsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
