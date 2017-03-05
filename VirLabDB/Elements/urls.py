03077df3b4f1f7b87cfe5f2e698214036b3569c0
Andres Morales <mora200217@hotmail.com>
2017-03-05 13:26:16 -0500

    A href done. Pk connection

--- VirLabDB/Elements/urls.py
+++ VirLabDB/Elements/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.hello_world),
+    url(r'^$', views.hello_world, name = 'hello'),
+    url(r'^product/(?P<pk>[0-9]+)/$', views.product_detail, name='product_detail')
 ]
--- VirLabDB/Elements/views.py
+++ VirLabDB/Elements/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
@@ -10,4 +10,12 @@ def hello_world(request):
     context = {
         'products': product
     }
-    return HttpResponse(template.render(context,request))
+    return HttpResponse(template.render(context, request))
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    template = loader.get_template('productDetail.html')
+    context = {
+        'product': product
+    }
+    return HttpResponse(template.render(context, request))
