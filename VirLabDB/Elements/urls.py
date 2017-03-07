5639c7f8e0a85b7d8eb64b833b05481c1feb7a07
Andres Morales <mora200217@hotmail.com>
2017-03-06 19:44:12 -0500

    Form Added. Urls modified. Css small debugs

--- VirLabDB/Elements/urls.py
+++ VirLabDB/Elements/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.hello_world, name = 'hello'),
-    url(r'^product/(?P<pk>[0-9]+)/$', views.product_detail, name='product_detail')
+    url(r'^product/(?P<pk>[0-9]+)/$', views.product_detail, name='product_detail'),
+    url(r'^product/new', views.new_product, name = 'new_product')
 ]
--- VirLabDB/Elements/views.py
+++ VirLabDB/Elements/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 
 from .models import Product
+from .forms import ProductForm
 
 def hello_world(request):
     product = Product.objects.order_by('id')
@@ -17,5 +18,22 @@ def product_detail(request, pk):
     template = loader.get_template('productDetail.html')
     context = {
         'product': product
+    }
+    return HttpResponse(template.render(context, request))
+
+def new_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            product = form.save()
+            product.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = ProductForm()
+
+
+    template = loader.get_template('newProduct.html')
+    context = {
+        'form': form
     }
     return HttpResponse(template.render(context, request))
